2f4e0ba846d55bac735a58428a227b33df593a54
Paul A. Medeiros <medeirospa@gmail.com>
2023-04-05 09:12:24 -0400

    Add function fish_user_key_bindings to file fish_user_key_bindings.

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
-function fish_user_key_bindings
 bind -M insert \ea end-of-line
+function fish_user_key_bindings
 #bind -M insert -m default jj backward-char force-repaint
-fzf_key_bindings
+    fzf_key_bindings
 end
