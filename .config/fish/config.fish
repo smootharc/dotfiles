c4480889b3de3a0cf77e87a52ab477f59256b189
Paul A. Medeiros <medeirospa@gmail.com>
2023-08-30 06:20:43 -0400

    Add various abbreviations. Add /sbin to path.  Add mistory merge.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
-bind -M insert \ea end-of-line
-history merge
+bind \ct '__fzf_find_file'
 abbr -a -- tree 'exa --tree'
 abbr -a -- l exa
 abbr -a -- ll 'exa -lg'
@@ -10,7 +9,6 @@ abbr -a -- scu 'systemctl --user'
 abbr -a -- jc journalctl
 abbr -a -- jcu 'journalctl --user'
 abbr -a -- rs 'restic snapshots'
-abbr -a -- rc 'restic check'
 abbr -a -- rl 'restic ls' 
 abbr -a -- rll 'restic ls latest &| less'
 abbr -a -- hdm 'udisksctl mount -b'
@@ -18,7 +16,10 @@ abbr -a -- hdu 'udisksctl unmount -b'
 abbr -a -- hds 'udisksctl status'
 abbr -a -- we 'curl wttr.in/08096'
 abbr -a -- wfs 'wf select'
-abbr -a -- skd 'for d in /dev/sd?; doas skdump $d; end | bat'
-set fzfkeys = "/usr/share/fzf/shell/key-bindings.fish"
-test -e "$fzfkeys" && source "$fzfkeys"
-bind \ct '__fzf_find_file'
+abbr -a -- skd 'for d in /dev/sd?; doas skdump $d; end | /usr/bin/bat'
+abbr -a -- rc 'journalctl --user -ru restic-check-local --since today'
+abbr -a -- avp 'rclone sync ~ /backup/avp --filter-from ~/.config/rclone/avp.fil -P'
+history merge
+if not contains /sbin $PATH
+    fish_add_path -aP /sbin
+end
