792d587a19e9d27f4fae54a2e1370844492c64d3
Paul A. Medeiros <medeirospa@gmail.com>
2024-06-18 19:09:19 -0400

    Too many changes to list.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,28 @@
-bind \ct '__fzf_find_file'
-abbr -a -- tree 'exa --tree'
-abbr -a -- l exa
-abbr -a -- ll 'exa -lg'
-abbr -a -- la 'exa -a'
-abbr -a -- lla 'exa -lag'
-abbr -a --position anywhere -- sc 'systemctl'
+bind \ct __fzf_find_file
+abbr -a -- ef 'exec fish'
+abbr -a -- tree 'tree -n'
+abbr -a -- hx helix
+abbr -a --position anywhere -- sc systemctl
 abbr -a -- scu 'systemctl --user'
+abbr -a -- frl 'source ~/.config/fish/config.fish'
 abbr -a -- jc journalctl
 abbr -a -- jcu 'journalctl --user'
-abbr -a -- rs 'restic snapshots'
-abbr -a -- rl 'restic ls' 
+abbr -a -- rs 'restic snapshots | bat'
 abbr -a -- rll 'restic ls latest &| less'
+abbr -a -- hdf 'df -hT -x efivarfs -x tmpfs -x devtmpfs'
 abbr -a -- hdm 'udisksctl mount -b'
 abbr -a -- hdu 'udisksctl unmount -b'
 abbr -a -- hds 'udisksctl status'
 abbr -a -- we 'curl wttr.in/08096'
 abbr -a -- wfs 'wf select'
-abbr -a -- skd 'for d in /dev/sd?; doas skdump $d; end | /usr/bin/bat'
-abbr -a -- rc 'journalctl --user -ru restic-check-local --since today'
+abbr -a -- skd 'for d in /dev/sd?; doas skdump $d; end | /usr/bin/less -p dev/sd'
 abbr -a -- avp 'rclone sync ~ /backup/avp --filter-from ~/.config/rclone/avp.fil -P'
+abbr -a -- pics 'feh &> /dev/null (fd . ~/Pictures -t f | fzf -m) &'
+abbr -a -- docs 'zathura (fd . -e pdf -e ps -t f ~ | fzf -m || exit ) &'
+# abbr -a -- docs 'zathura (find ~ -iname "*.pdf" -o -iname "*.ps" | fzf -m || exit ) & '
+abbr -a -- bur 'test -d ~/media/backup && rclone sync /backup/restic ~/media/backup/restic -P && umount ~/media/backup'
 history merge
-if not contains /sbin $PATH
-    fish_add_path -aP /sbin
-end
+zoxide init fish | source
+#if not contains /sbin $PATH
+#    fish_add_path -aP /sbin
+#end
--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,4 @@
 # Defined in /tmp/fish.6eMpCI/fish_right_prompt.fish @ line 2
 function fish_right_prompt
-
-    date -R
+    #    date
 end
