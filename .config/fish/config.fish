0ee7424e41eb8b3ba7880e3547453394bffe4eae
Paul A. Medeiros <medeirospa@gmail.com>
2024-11-15 09:23:49 -0500

    Minor changes to config.fish and bp.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,20 @@
 # bind \ct __fzf_find_file
+abbr -a -- avp 'rclone sync ~ /backup/avp --filter-from ~/.config/rclone/avp.fil -P'
+abbr -a -- bur 'test -d ~/media/backup && rclone sync /backup/restic ~/media/backup/restic -P && umount ~/media/backup'
 abbr -a -- ef 'exec fish'
-abbr -a -- tree 'eza --tree'
+abbr -a -- hdm 'udisksctl mount -b'
+abbr -a -- hds 'udisksctl status'
+abbr -a -- hdu 'udisksctl unmount -b'
 abbr -a -- hx helix
-abbr -a --position anywhere -- sc systemctl
-abbr -a -- scu 'systemctl --user'
 abbr -a -- jc journalctl
 abbr -a -- jcu 'journalctl --user'
-abbr -a -- rs 'restic snapshots'
+abbr -a -- pics 'feh &> /dev/null (fd . ~/Pictures -t f | fzf -m) &'
 abbr -a -- rll 'restic ls latest &| less'
-abbr -a -- hdm 'udisksctl mount -b'
-abbr -a -- hdu 'udisksctl unmount -b'
-abbr -a -- hds 'udisksctl status'
+abbr -a -- rs 'restic snapshots'
+abbr -a -- scu 'systemctl --user'
+abbr -a -- tree 'eza --tree'
 abbr -a -- we 'curl wttr.in/08096'
 abbr -a -- wfs 'wf select'
-abbr -a -- avp 'rclone sync ~ /backup/avp --filter-from ~/.config/rclone/avp.fil -P'
-abbr -a -- pics 'feh &> /dev/null (fd . ~/Pictures -t f | fzf -m) &'
-abbr -a -- bur 'test -d ~/media/backup && rclone sync /backup/restic ~/media/backup/restic -P && umount ~/media/backup'
-abbr -a -- smartlog 'journalctl -b -eu smartd'
+abbr -a --position anywhere -- sc systemctl
 history merge
 zoxide init fish | source
