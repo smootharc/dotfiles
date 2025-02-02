f088c70d1a93c58bb01a63c21bee201cace5fa8d
Paul A. Medeiros <medeirospa@gmail.com>
2025-02-02 05:04:47 -0500

    Many modfications.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ abbr -a -- hdu 'udisksctl unmount -b'
 abbr -a -- hx helix
 abbr -a -- jc journalctl
 abbr -a -- jcu 'journalctl --user'
+abbr -a -- jcup 'journalctl --user -t paul'
 abbr -a -- pics 'feh &> /dev/null (fd . ~/Pictures -t f | fzf -m) &'
 abbr -a -- rll 'restic ls latest &| less'
 abbr -a -- rs 'restic snapshots'
@@ -18,3 +19,9 @@ abbr -a -- wfs 'wf select'
 abbr -a --position anywhere -- sc systemctl
 history merge
 zoxide init fish | source
+if not string match -q /home/paul/.local/bin -- $PATH
+    set -gx PATH /home/paul/.local/bin $PATH
+end
+if not string match -q /home/paul/.nimble/bin -- $PATH
+    set -gx PATH /home/paul/.nimble/bin $PATH
+end
